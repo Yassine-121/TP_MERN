68fcef18d78e026bf7e9e42d11dba031484efddb
Yassine-121 <lyazighi.yassine@gmail.com>
2021-03-21 18:07:10 +0100

    Mise en place de React Router et les components

--- projet/src/App.js
+++ projet/src/App.js
@@ -1,27 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
 import './App.css';
+
+import Navbar from './components/navbar.component';
+import UserList from './components/user-list.component';
+import EditUser from './components/edit-user.component';
+import CreateUser from './components/create-user.component';
+import RemoveUser from './components/remove-user.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Yassine</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar/>
+        <Route path="/users" exact component={UserList}/>
+        <Route path="/users/:id" component={EditUser}/>
+        <Route path="/users" component={CreateUser}/>
+        <Route path="/users/:id" component={RemoveUser}/>
+
+      </div>
+    </Router>
   );
 }
 
